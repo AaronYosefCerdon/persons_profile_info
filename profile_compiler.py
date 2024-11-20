ce0fb5e15004b17516aeb4dac41976b7f28ddc6a
AaronYosefCerdon <aaronyosefcerdon123@gmail.com>
2024-11-20 14:08:19 +0800

    Initial Pseudocode Steps

---
+++ profile_compiler.py
@@ -0,0 +1,9 @@
+#Define a variable that will store the data collected for each person 
+
+#Use a loop to repeatedly ask for inputs person.
+
+#Ask the user if they want to continue.
+
+#Use read and write to put in all entries in a txt file.
+
+#Print the outputs to ensure the format.
