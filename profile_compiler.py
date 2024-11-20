6ffacdcd7d5d9e19b672805872ee888a33e3f476
AaronYosefCerdon <aaronyosefcerdon123@gmail.com>
2024-11-20 21:00:57 +0800

    Asking user for input

--- profile_compiler.py
+++ profile_compiler.py
@@ -1,5 +1,16 @@
-#Define a variable that will store the data collected for each person 
-
+#Define variables that will store the data collected for each person 
+full_name = input("Please input your full name: ")
+age = int(input("Please input your age: "))
+contact_number = int(input("Please input your contact number: "))
+address = input("Please input your address: ")
+email = input("Please input your email address: ")
+eye_color = input("Please input your eye color: ")
+favorite_color = input("Please input your favorite color: ")
+favorite_food = input("Please input your favorite food: ")
+dream_job = input("Please state your dream job: ")
+mbti_result = input("Please input your MBTI result: ")
+birthday = input("Please input your date of birth: ")
+zodiac_sign = input("Please input your zodiac sign: ")
 #Use a loop to repeatedly ask for inputs person.
 
 #Ask the user if they want to continue.
